93c8113f276722a9524c5699d02153ef750f3dcd
spacegateir <1Spacegateir@gmail.com>
2025-01-31 17:37:13 +1030

    Initial commit

---
+++ src/main/java/net/spacegateir/arcaness/ArcaneSteppingStonesClient.java
@@ -0,0 +1,10 @@
+package net.spacegateir.arcaness;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcaneSteppingStonesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/spacegateir/arcaness/ArcaneSteppingStonesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.spacegateir.arcaness;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArcaneSteppingStonesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
